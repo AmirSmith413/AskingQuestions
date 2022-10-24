e28b296050668b951ca823b5b069516fe5f078f9
AmirSmith413 <116046232+AmirSmith413@users.noreply.github.com>
2022-10-24 11:41:34 -0700

    Added the option the play again

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿
-
-
 
 //setting up the variables...
 int time;
 string getTime;
 string myName;
+string decision;
+bool playAgain = true;
 
-
-
+while(playAgain == true)
+{
 Console.WriteLine("What is your name?");
 
 //user will type their name
@@ -23,3 +22,20 @@ time = Convert.ToInt32(getTime);
 
 //name and time will be displayed here
 Console.WriteLine($"Your name is {myName} and you woke up at {time} o clock.");
+Console.WriteLine("Would you like to try again?Type Y to try again or N to end.");
+ decision = Console.ReadLine().ToUpper();
+ //if the user wanted to try again, pressing "Y" will send them to the beginning
+ if(decision == "Y")
+    {
+      playAgain = true;
+    }
+    else if(decision == "N")
+    {
+      //if the user wanted to stop pressing "N" will end the program
+      playAgain = false;
+    }
+    else
+    {
+        Console.WriteLine("Please pick a valid option.");
+    }   
+}
