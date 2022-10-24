36f071ddd56acde0562b05a617198fea11a5c6a8
AmirSmith413 <amirsmith166@gmail.com>
2022-10-24 11:37:54 -0700

    added the code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+//setting up the variables...
+int time;
+string getTime;
+string myName;
+
+
+
+Console.WriteLine("What is your name?");
+
+//user will type their name
+myName = Console.ReadLine();
+Console.WriteLine("What time did you wake up?");
+//user will type the time they woke up
+getTime = Console.ReadLine();
+
+//the int will convert into a string
+time = Convert.ToInt32(getTime);
+
+
+//name and time will be displayed here
+Console.WriteLine($"Your name is {myName} and you woke up at {time} o clock.");
